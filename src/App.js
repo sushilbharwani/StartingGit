0ed70b17906bd110dab98a9cb08b9bad6b84cd7e
Bharwani <sb185107@ncr.com>
2021-04-29 15:34:25 +0530

    added angular conflict

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React With Git
+          Learn Angular With Git
           Welcome to This Course
         </a>
       </header>
